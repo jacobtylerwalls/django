1e0232f21f42790db3f7ac09a0d6fa7a5fcc03c9
Jacob Walls <jacobtylerwalls@gmail.com>
2026-03-27 23:51:36 -0400

    Revert "Fixed #36770?"
    This reverts commit 3e72c239461b3eaabbffda5130ab41563f4cc53d.

--- django/test/testcases.py
+++ django/test/testcases.py
@@ -1783,12 +1783,11 @@ class LiveServerThread(threading.Thread):
         )
 
     def terminate(self):
-        if self.is_ready.is_set():
-            if hasattr(self, "httpd"):
-                # Stop the WSGI server
-                self.httpd.shutdown()
-                self.httpd.server_close()
-            self.join()
+        if hasattr(self, "httpd"):
+            # Stop the WSGI server
+            self.httpd.shutdown()
+            self.httpd.server_close()
+        self.join()
 
 
 class LiveServerTestCase(TransactionTestCase):
